eab03bf00588b8db75a6c1b5fcbe29479b7ce036
Rishi860 <rish860official@gmail.com>
2020-07-18 20:06:25 +0530

    Search Bar added

---
+++ client/src/views/Songs/SongSearchPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <panel title="Search">
+    <div slot="action" >
+      <v-btn class="mx-2"
+        fab
+        dark 
+        color="#3D5AFE"
+        @click="navigateTo({name:'create-songs'})">
+        <v-icon dark>mdi-magnify</v-icon>
+      </v-btn>
+    </div>
+    <v-text-field label="Search song by song title, artist, or genre" v-model="search">
+    </v-text-field>
+  </panel>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
